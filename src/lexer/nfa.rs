2a68c7df5e4c8536f5169153be918e784ac54c34
Jeremy Powell <jeremy.wayne.powell@gmail.com>
2024-12-11 11:44:53 -0600

    added epsilon, intersect, complement, and others to regex

--- src/lexer/nfa.rs
+++ src/lexer/nfa.rs
@@ -3,3 +3,9 @@ pub struct NfaBuilder<T>(std::marker::PhantomData<T>);
 impl<T> NfaBuilder<T> {}
 
 pub struct Nfa<T>(std::marker::PhantomData<T>);
+
+impl<T> Nfa<T> {
+    pub fn new() -> Self {
+        Self(std::marker::PhantomData)
+    }
+}
--- src/lexer/regex.rs
+++ src/lexer/regex.rs
@@ -1,9 +1,30 @@
+use std::rc::Rc;
+
 use super::nfa::Nfa;
 
-pub struct Regex;
+#[derive(Clone)]
+pub struct Regex {
+    node: Rc<RegexNode>,
+    nullable: bool,
+}
+
+pub enum RegexNode {
+    Empty,
+    Epsilon,
+    Range(char, char),
+    Concat(Regex, Regex),
+    Union(Regex, Regex),
+    Intersect(Regex, Regex),
+    Star(Regex),
+    Not(Regex),
+}
 
 pub fn empty() -> Regex {
     Regex::empty()
+}
+
+pub fn epsilon() -> Regex {
+    Regex::epsilon()
 }
 
 pub fn any() -> Regex {
@@ -27,56 +48,98 @@ pub fn none_of(chars: &str) -> Regex {
 }
 
 impl Regex {
+    pub fn is_empty(&self) -> bool {
+        matches!(*self.node, RegexNode::Empty)
+    }
+
+    pub fn is_epsilon(&self) -> bool {
+        matches!(*self.node, RegexNode::Epsilon)
+    }
+
     pub fn empty() -> Self {
-        todo!()
+        Self {
+            node: Rc::new(RegexNode::Empty),
+            nullable: false,
+        }
+    }
+
+    pub fn epsilon() -> Self {
+        Self {
+            node: Rc::new(RegexNode::Epsilon),
+            nullable: true,
+        }
     }
 
     pub fn any() -> Self {
-        todo!()
+        Self::range(char::MIN, char::MAX)
     }
 
     pub fn char(c: char) -> Self {
-        todo!()
+        Self::range(c, c)
     }
 
     pub fn range(lo: char, hi: char) -> Self {
-        todo!()
+        Self {
+            node: Rc::new(RegexNode::Range(lo, hi)),
+            nullable: false,
+        }
     }
 
     pub fn concat(&self, other: &Self) -> Self {
-        todo!()
+        Self {
+            node: Rc::new(RegexNode::Concat(self.clone(), other.clone())),
+            nullable: self.nullable && other.nullable,
+        }
     }
 
     pub fn union(&self, other: &Self) -> Self {
-        todo!()
+        Self {
+            node: Rc::new(RegexNode::Union(self.clone(), other.clone())),
+            nullable: self.nullable || other.nullable,
+        }
     }
 
     pub fn intersect(&self, other: &Self) -> Self {
-        todo!()
+        Self {
+            node: Rc::new(RegexNode::Intersect(self.clone(), other.clone())),
+            nullable: self.nullable && other.nullable,
+        }
     }
 
     pub fn star(&self) -> Self {
-        todo!()
+        Self {
+            node: Rc::new(RegexNode::Star(self.clone())),
+            nullable: true,
+        }
     }
 
     pub fn plus(&self) -> Self {
-        todo!()
+        self.concat(&self.star())
     }
 
     pub fn opt(&self) -> Self {
-        todo!()
+        self.union(&Self::epsilon())
     }
 
     pub fn not(&self) -> Self {
-        todo!()
+        Self {
+            node: Rc::new(RegexNode::Not(self.clone())),
+            nullable: !self.nullable,
+        }
     }
 
     pub fn one_of(chars: &str) -> Self {
-        todo!()
+        let mut r = Self::empty();
+
+        for c in chars.chars() {
+            r = r.union(&Self::char(c));
+        }
+
+        r
     }
 
     pub fn none_of(chars: &str) -> Self {
-        todo!()
+        Self::one_of(chars).not()
     }
 
     pub fn nfa<T: Clone>(&self, output: T) -> Nfa<T> {
